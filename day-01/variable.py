0d3c52aa78d99563c5e8f851b424a009b998a188
Birukmengistu <sheger.shelf3@gmail>
2024-09-25 18:55:41 +0200

    check last update

---
+++ day-01/variable.py
@@ -0,0 +1,8 @@
+print(f'2o day are {20*24*60 *60} second')
+print(f'2o day are {20*24*60} minutes')
+print(f'2o day are {20*24} hour')
+
+
+to_min_2o_days = 24*60 *60
+name_of_unit = 'mint'
+print(f'2o day are {to_min_2o_days} {name_of_unit}')
