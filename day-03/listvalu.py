a0f3f65b3bbc4b53a817657aa3dc42e46f654693
Birukmengistu <sheger.shelf3@gmail>
2024-09-27 09:33:40 +0200

    day-03 task list tuples loop

--- day-03/listvalu.py
+++ day-03/listvalu.py
@@ -10,18 +10,3 @@ def displayNumberSecondOfdays(value, unit):
    print(f'20 day are {value * hour *Miuntes} {unit}')
 
 
-def validate_and_ect (user_input):
-    
-    if int(user_input) >0:
-      return display_no_min_day(user_input,name_of_unit[1])
-    elif int(user_input) == 0:
-      return display_no_min_day(user_input,name_of_unit[2])
-    else:
-       return f'user input not valid'
-    
-
-input_value=''
-while input_value != 'exist':
-   user_input = input('inter value \n')
-   for x in user_input.split():
-     validate_and_ect(x)
